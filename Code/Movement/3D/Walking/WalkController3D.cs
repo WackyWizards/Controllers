d445428d68e461a1d2dabeac15a91adeb1907f0c
kEllieDev <kEllieDev@users.noreply.github.com>
2025-09-24 20:08:48 +0300

    Move folders for 3D controllers

--- Code/Movement/3D/Walking/WalkController3D.cs
+++ Code/Movement/3D/Walking/WalkController3D.cs
@@ -41,9 +41,6 @@ public partial class WalkController3D : MovementController3D
 	[Property]
 	private float SkinWidth { get; set; } = 0.1f;
 
-	[Property]
-	private bool Debug { get; set; }
-
 	private BoxCollider FloorCollider { get; set; }
 	private float GroundRayDistance { get; set; } = 2f;
 
@@ -373,25 +370,5 @@ public partial class WalkController3D : MovementController3D
 		AnimationHelper.WithLook( EyeAngles.Forward * 100 );
 		AnimationHelper.WithVelocity( Velocity );
 		AnimationHelper.WithWishVelocity( WishVelocity );
-	}
-	
-	private void DrawDebugInfo()
-	{
-		Gizmo.Draw.ScreenText( $"IsGrounded: {IsGrounded}", new Vector2( 10, 10 ), size: 20 );
-		Gizmo.Draw.ScreenText( $"Velocity: {Velocity}", new Vector2( 10, 30 ), size: 20 );
-		Gizmo.Draw.ScreenText( $"WishVelocity: {WishVelocity}", new Vector2( 10, 50 ), size: 20 );
-		Gizmo.Draw.ScreenText( $"Speed: {_speed}", new Vector2( 10, 70 ), size: 20 );
-
-		if ( CanJump )
-		{
-			Gizmo.Draw.ScreenText( $"WishJump: {WishJump}", new Vector2( 10, 90 ), size: 20 );
-			Gizmo.Draw.ScreenText( $"Coyote Time: {_coyoteTime}", new Vector2( 10, 110 ), size: 20 );
-			Gizmo.Draw.ScreenText( $"Jump Buffer: {_jumpBuffer}", new Vector2( 10, 130 ), size: 20 );
-		}
-
-		if ( CanCrouch )
-		{
-			Gizmo.Draw.ScreenText( $"WishCrouch: {WishCrouch}", new Vector2( 10, 150 ), size: 20 );
-		}
 	}
 }
